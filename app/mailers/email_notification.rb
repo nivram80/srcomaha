831a05ca350ac88f17f134bb44bcd61e25c16f65
Todd Nichols <nivram80@gmail.com>
2014-11-22 23:02:18 -0600

    reformat the notification email

--- app/mailers/email_notification.rb
+++ app/mailers/email_notification.rb
@@ -1,10 +1,10 @@
 class EmailNotification < ActionMailer::Base
-  default from: "lnichols9791@gmail.com"
+  default from: "contact@srcomaha.net"
 
   def send_contact_us_email(email)
   	@email = email
   	mail( to: 'nivram80@gmail.com',
-  				subject: 'SRCOmaha.net Contact Us Email')
+  				subject: 'SrcOmaha.net Contact Us Email')
   end
 
 end
